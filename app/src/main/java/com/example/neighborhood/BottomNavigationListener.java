cf49d8d59a52d3fc5fe0bf940382089d0d8df384
adee9 <adee.99uk@yahoo.com>
2023-07-19 19:47:34 +0100

    added fragments

---
+++ app/src/main/java/com/example/neighborhood/BottomNavigationListener.java
@@ -0,0 +1,54 @@
+package com.example.neighborhood;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.neighborhood.Fragment.HomeFragment;
+import com.example.neighborhood.Fragment.CommunityFragment;
+import com.example.neighborhood.Fragment.AddFragment;
+import com.example.neighborhood.Fragment.EventFragment;
+import com.example.neighborhood.Fragment.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class BottomNavigationListener implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private final Context context;
+
+    public BottomNavigationListener(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment selectedFragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                selectedFragment = new HomeFragment();
+                break;
+            case R.id.navigation_community:
+                selectedFragment = new CommunityFragment();
+                break;
+            case R.id.navigation_add:
+                selectedFragment = new AddFragment();
+                break;
+            case R.id.navigation_event:
+                selectedFragment = new EventFragment();
+                break;
+            case R.id.navigation_profile:
+                selectedFragment = new ProfileFragment();
+                break;
+        }
+
+        if (selectedFragment != null) {
+            ((MainActivity) context).getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, selectedFragment)
+                    .commit();
+        }
+
+        return true;
+    }
+}
--- app/src/main/java/com/example/neighborhood/MainActivity.java
+++ app/src/main/java/com/example/neighborhood/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.neighborhood;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.example.neighborhood.Fragment.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -46,6 +49,12 @@ public class MainActivity extends AppCompatActivity {
         } else {
             textView.setText("Logged in as: " + user.getEmail());
         }
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationListener(this));
+
+        // Set the initial selected item to Home
+        bottomNavigationView.setSelectedItemId(R.id.navigation_home);
 
         buttonProfile.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -106,4 +115,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+////    @Override
+//    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//        Fragment selectedFragment = null;
+//
+//        switch (item.getItemId()) {
+//            case R.id.navigation_home:
+//                selectedFragment = new HomeFragment();
+//                break;
+//            case R.id.navigation_community:
+//                // Handle click for Community icon
+//                // Add your code here for the Community screen or fragment
+//                break;
+//            case R.id.navigation_add:
+//                // Handle click for Add icon
+//                // Add your code here for the Add screen or fragment
+//                break;
+//            case R.id.navigation_event:
+//                // Handle click for Event icon
+//                // Add your code here for the Event screen or fragment
+//                break;
+//            case R.id.navigation_profile:
+//                // Handle click for Profile icon
+//                Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+//                startActivity(intent);
+//                finish();
+//                break;
+//        }
+//
+//        if (selectedFragment != null) {
+//            getSupportFragmentManager().beginTransaction()
+//                    .replace(R.id.fragment_container, selectedFragment)
+//                    .commit();
+//        }
+//
+//        return true;
+//    }
 }
