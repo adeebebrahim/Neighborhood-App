033528f4be07ab40d595f84bf89f4115f03d61be
adee9 <adee.99uk@yahoo.com>
2023-08-16 13:17:41 +0100

    follow button is working properly

--- app/src/main/java/com/example/neighborhood/Fragment/OtherUserProfileFragment.java
+++ app/src/main/java/com/example/neighborhood/Fragment/OtherUserProfileFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,10 +23,13 @@ import com.example.neighborhood.Adapter.PostAdapter;
 import com.example.neighborhood.Post;
 import com.example.neighborhood.R;
 import com.example.neighborhood.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
@@ -47,6 +51,8 @@ public class OtherUserProfileFragment extends Fragment implements PostAdapter.Us
     private List<Post> postList;
 
     private String otherUserId; // User ID of the other user
+    private String currentUserId; // User ID of the current user
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -59,6 +65,10 @@ public class OtherUserProfileFragment extends Fragment implements PostAdapter.Us
         followingTextView = rootView.findViewById(R.id.following_text);
         profileImageView = rootView.findViewById(R.id.profile_image);
         postRecyclerView = rootView.findViewById(R.id.posts_recycler_view);
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        currentUserId = currentUser != null ? currentUser.getUid() : null;
+
 
         // Retrieve the other user's ID from arguments
         Bundle bundle = getArguments();
@@ -118,6 +128,15 @@ public class OtherUserProfileFragment extends Fragment implements PostAdapter.Us
                         // If profile image URL is empty, load a default image
                         Glide.with(requireContext()).load(R.drawable.ic_profile).into(profileImageView);
                     }
+
+                    // Check if the current user is following the other user
+                    boolean isFollowing = false; // Initialize as false
+                    if (currentUserId != null && otherUser.getFollowers() != null) {
+                        isFollowing = otherUser.getFollowers().containsKey(currentUserId);
+                    }
+
+                    // Update the follow button text and behavior
+                    updateFollowButton(isFollowing);
                 }
             }
 
@@ -127,6 +146,62 @@ public class OtherUserProfileFragment extends Fragment implements PostAdapter.Us
             }
         });
     }
+
+
+    private void updateFollowButton(boolean isFollowing) {
+        Button followButton = getView().findViewById(R.id.follow_button);
+        followButton.setText(isFollowing ? "Following" : "Follow");
+
+        followButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isFollowing) {
+                    // Unfollow the user
+                    unfollowUser();
+                } else {
+                    // Follow the user
+                    followUser();
+                }
+            }
+        });
+    }
+
+    private void followUser() {
+        DatabaseReference currentUserRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserId);
+        DatabaseReference otherUserRef = FirebaseDatabase.getInstance().getReference().child("Users").child(otherUserId);
+
+        // Add the other user's ID to the following list of the current user
+        currentUserRef.child("following").child(otherUserId).setValue(true);
+
+        // Add the current user's ID to the followers list of the other user
+        otherUserRef.child("followers").child(currentUserId).setValue(true);
+
+        // Increment the followerCount of the other user and the followingCount of the current user
+        otherUserRef.child("followerCount").setValue(ServerValue.increment(1));
+        currentUserRef.child("followingCount").setValue(ServerValue.increment(1));
+
+        // Update UI and button text
+        updateFollowButton(true);
+    }
+
+    private void unfollowUser() {
+        DatabaseReference currentUserRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserId);
+        DatabaseReference otherUserRef = FirebaseDatabase.getInstance().getReference().child("Users").child(otherUserId);
+
+        // Remove the other user's ID from the following list of the current user
+        currentUserRef.child("following").child(otherUserId).removeValue();
+
+        // Remove the current user's ID from the followers list of the other user
+        otherUserRef.child("followers").child(currentUserId).removeValue();
+
+        // Decrement the followerCount of the other user and the followingCount of the current user
+        otherUserRef.child("followerCount").setValue(ServerValue.increment(-1));
+        currentUserRef.child("followingCount").setValue(ServerValue.increment(-1));
+
+        // Update UI and button text
+        updateFollowButton(false);
+    }
+
 
     private void loadOtherUserPosts() {
         DatabaseReference postsRef = FirebaseDatabase.getInstance().getReference().child("posts");
